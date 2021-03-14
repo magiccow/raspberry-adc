fd2f2375b73f9eb7551067457a94b76c1157e5a9
Martyn Davies <martyn@magiccow.co.uk>
2021-03-14 14:11:42 +0000

    initial commit

---
+++ read-ad.py
@@ -0,0 +1,11 @@
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+chan = AnalogIn(ads, ADS.P0)
+
+print("Value = {}".format(chan.value))
+print("Voltage = {}".format(chan.voltage))
